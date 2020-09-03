62312ed996d20bb1d8bfdd537d000920d602324a
Ankit Kumar <panditankit1995@gmail.com>
2020-09-03 10:48:39 +0530

    🎨 new gradients added

--- lib/theme/gradients.dart
+++ lib/theme/gradients.dart
@@ -18,6 +18,26 @@ class GradientValues {
       to: Color(0xFF36d1dc),
     ),
     Gradient(
+      name: "50 Shades of Grey",
+      from: Color(0xFF2c3e50),
+      to: Color(0xFFbdc3c7),
+    ),
+    Gradient(
+      name: "Pale Wood",
+      from: Color(0xFFd6ae7b),
+      to: Color(0xFFeacda3),
+    ),
+    Gradient(
+      name: "Cherry",
+      from: Color(0xFFeb3349),
+      to: Color(0xFFf45c43),
+    ),
+    Gradient(
+      name: "Green Beach",
+      from: Color(0xFF02aab0),
+      to: Color(0xFF00cdac),
+    ),
+    Gradient(
       name: "Aubergine",
       from: Color(0xFFaa076b),
       to: Color(0xFF61045f),
